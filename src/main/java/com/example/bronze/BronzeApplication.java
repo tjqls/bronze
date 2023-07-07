194078cb75c1ec6568606fbdd0adb1f5e13b878a
tjqls <tjqls2013@gmail.com>
2023-07-07 09:16:04 +0900

    연결

---
+++ src/main/java/com/example/bronze/BronzeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bronze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BronzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BronzeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bronze/BronzeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bronze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BronzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
